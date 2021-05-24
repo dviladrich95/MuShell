54a395ee808563b7e23a031b4cfac101564557f2
dviladrich95 <daniel.viladrich.h@gmail.com>
2021-05-24 09:20:40 +0200

    working on fourier function, added some docs

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import cv2 as cv
-#import mido
+# import mido
 from matplotlib import pyplot as plt
+from scipy.interpolate import interp1d
 import numpy as np
+from scipy.fft import fft
 
 
 def threshold_test(img):
     """
-    :param img:
+    Test function that converts RGB image tho thresholded counterpart, not really used right now
+    :param img: image to be thresholded
     :return:
-
     """
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     img_lap = cv.Laplacian(img_gray, cv.CV_64F, ksize=5)
@@ -26,7 +28,7 @@ def threshold_test(img):
 def count_objects(img_thresh):
     """
     Counts the number of objects in the binary image
-    :param img_thresh:
+    :param img_thresh: thresholded image
     :return:
     """
     label_image = img_thresh.copy()
@@ -42,13 +44,16 @@ def count_objects(img_thresh):
 
     return label_count, label_image
 
-def quantize_image(img_thresh,box_list,qbox_list):
+
+def quantize_image(img_thresh, box_list, qbox_list):
     """
     Counts the number of objects in the binary image
+    :param box_list:
+    :param qbox_list:
     :param img_thresh:
     :return:
     """
-    box_diff_list = (box_list-qbox_list)//1
+    box_diff_list = (box_list - qbox_list) // 1
 
     label_image = img_thresh.copy()
     label_count = 0
@@ -62,69 +67,88 @@ def quantize_image(img_thresh,box_list,qbox_list):
                 cv.floodFill(label_image, None, (i, j), label_count)
 
     return label_count, label_image
+
 
 def get_boxes(contours):
     """
     Counts the number of points in the image and returns a list of the fitting box parameters associated with each
     box: (length,height,x_center,y_center)
-    :param img_thresh:
+    :param contours:
     :return:
     """
 
-    boxes = []
+    box_list = []
     for ctr in contours:
         x, y, w, h = cv.boundingRect(ctr)
-        boxes.append([x, y, w, h])
-    boxes = np.asarray(boxes)
-    return boxes
+        box_list.append([x, y, w, h])
+    box_list = np.asarray(box_list)
+    return box_list
 
-def quantize_time(box_list,img_height,time_divisions=120): #30 seconds at 120 bpm
+
+def quantize_time(box_list, img_height, time_divisions=120):  # 30 seconds at 120 bpm
     """
-
+    Quantizes the time coordinate (y coordinate) of the picture
     :param box_list:
     :param img_height:
     :param time_divisions:
     :return:
     """
-    quant_param= img_height/time_divisions
-    qtime_list = box_list[:,1]//quant_param
-    qtime_list_multiplied = qtime_list*int(quant_param)
+    quant_param = img_height / time_divisions
+    qtime_list_boxnum = box_list[:, 1] / quant_param
+    qtime_list = qtime_list_boxnum * int(quant_param)
     return qtime_list
 
 
-def quantize_pitch(box_list,img_width,note_num=18): #18 because its roughly the number of
-    quant_param= img_width/note_num
-    qpitch_list = box_list[:,0]//quant_param
-
+def quantize_pitch(box_list, img_width, note_num=18):  # 18 because its roughly the number of
+    """
+    Quantizes the pitch coordinate (x coordinate) of the picture
+    :param box_list:
+    :param img_width:
+    :param note_num:
+    :return:
+    """
+    quant_param = img_width / note_num
+    qpitch_list_boxnum = box_list[:, 0] // quant_param
+    qpitch_list = qpitch_list_boxnum * int(quant_param)
     return qpitch_list
 
-def contour2fourier(contours,n=1000):
+
+def contour2fourier(contours, n=100000,interpoints=100):
     """
     Convert contour pixel list into fourier descriptor
     """
-    descriptor_list=[]
-    for contour in contours:
-        complex_ctr = contour[:,0,0] + 1j * contour[:,0,1]
-        descriptor_complex=np.fft.fft(complex_ctr,axis=0,n=n)
-        descriptor_abs= np.abs(descriptor_complex)
-        descriptor = descriptor_abs[1:]/descriptor_abs[1]
+    descriptor_list = []
+    for i,contour in enumerate(contours):
+        #f_contour = interp1d(contour[0], contour[1], kind='cubic')
+        #contour_norm = f_contour(100)
+
+        x= np.linspace(0.0, 2* np.pi*n)/n
+        #c1 = np.sin(30.0 * 2.0 * np.pi * x) + 1j * np.sin(30.0 * 2.0 * np.pi * x)
+        #c2 = np.cos(60.0 * 2.0 * np.pi * x) + 1j * np.sin(60.0 * 2.0 * np.pi * x)
+        #complex_ctr = c1 + c2
+        complex_ctr = contour[:, 0, 0] + 1j * contour[:, 0, 1]
+        #descriptor_complex = np.fft.fft(complex_ctr, axis=0, n=n)
+        descriptor_complex = fft(complex_ctr, axis=0, n=n)
+        descriptor_abs = np.abs(descriptor_complex)
+        descriptor = descriptor_abs[1:] / descriptor_abs[1]
         descriptor_list.append(descriptor_abs)
 
-        n_list=range(1,n)
-        plt.plot(n_list,descriptor)
+        plt.title("Descriptor {} with {} pixels ".format(i,len(contour)))
+        plt.plot(range(1, n), descriptor[:n])
         plt.show()
 
     return descriptor_list
 
-def show_boxes(img_thresh, boxes):
-    """
 
+def show_boxes(img_thresh, box_list):
+    """
+    Show a plot with boxes around each dot
     :param img_thresh:
     :param boxes:
     :return:
     """
     img_thresh_rgb = cv.cvtColor(img_thresh.copy(), cv.COLOR_GRAY2RGB)
-    for box in boxes:
+    for box in box_list:
         top_left = (box[0], box[1])
         bottom_right = (box[0] + box[2], box[1] + box[3])
         cv.rectangle(img_thresh_rgb, top_left, bottom_right, (0, 255, 0), 2)
@@ -133,14 +157,16 @@ def show_boxes(img_thresh, boxes):
     cv.waitKey(0)
     return img_thresh_rgb
 
+
 def boxes2midi(box_params):
     """
-    Converts each list of box parameters into MIDI format (note, duration, loudness)
+    Converts each list of box parameters into MIDI format (note, duration, loudness) using the mido or pyaudio module
     """
+
 
 def midi2audio(box_params):
     """
-    Converts each list of box parameters into MIDI format (note, duration, loudness)
+    Converts each list of box parameters into MIDI format (note, duration, loudness) using the mido or pyaudio module
     """
 
 
@@ -162,16 +188,15 @@ if __name__ == '__main__':
     box_list = get_boxes(contours)
     descriptor_list = contour2fourier(contours)
 
-    qtime_list=quantize_time(box_list,img_height)
-    qpitch_list=quantize_pitch(box_list,img_width)
-    qbox_list=zip(qpitch_list,qtime_list)
-
+    qtime_list = quantize_time(box_list, img_height)
+    qpitch_list = quantize_pitch(box_list, img_width)
+    qbox_list = zip(qpitch_list, qtime_list)
 
     _ = show_boxes(img_thresh, box_list)
 
     # label_count, label_image = count_objects(img_thresh)
 
-    #label_count, label_image = quantize_image(img_thresh,box_list,qbox_list)
+    # label_count, label_image = quantize_image(img_thresh,box_list,qbox_list)
 
     # box_params=get_boxes(img)
     # midi_sequence=midi_convert(box_params)
