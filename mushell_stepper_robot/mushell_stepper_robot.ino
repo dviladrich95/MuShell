10e1976598732bfc98fbdab33894e3f84c12816e
dviladrich95 <daniel.viladrich.h@gmail.com>
2021-06-10 15:43:46 +0200

    roboter code cleanup

--- mushell_stepper_robot/mushell_stepper_robot.ino
+++ mushell_stepper_robot/mushell_stepper_robot.ino
@@ -4,6 +4,7 @@
 #define STEPPER_PIN_3 10
 #define STEPPER_PIN_4 11
 int step_number = 0;
+int num_steps = 70;
 void setup() {
 pinMode(STEPPER_PIN_1, OUTPUT);
 pinMode(STEPPER_PIN_2, OUTPUT);
@@ -13,73 +14,21 @@ pinMode(STEPPER_PIN_4, OUTPUT);
 }
 
 void loop() {
- 
-  OneStep(true);
-  delay(2);
-  
+  Step(num_steps);
+  delay(1000);
 
 }
-
-
-void OneStep(bool dir){
-    if(dir){
-switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
-} 
-  }else{
-    switch(step_number){
-  case 0:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, HIGH);
-  break;
-  case 1:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, HIGH);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 2:
-  digitalWrite(STEPPER_PIN_1, LOW);
-  digitalWrite(STEPPER_PIN_2, HIGH);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
-  break;
-  case 3:
-  digitalWrite(STEPPER_PIN_1, HIGH);
-  digitalWrite(STEPPER_PIN_2, LOW);
-  digitalWrite(STEPPER_PIN_3, LOW);
-  digitalWrite(STEPPER_PIN_4, LOW);
- 
+void Step(int num_steps){
+  for(int i=0; i<num_steps; i++){
+  digitalWrite(STEPPER_PIN_1, step_number==0);
+  digitalWrite(STEPPER_PIN_2, step_number==1);
+  digitalWrite(STEPPER_PIN_3, step_number==2);
+  digitalWrite(STEPPER_PIN_4, step_number==3);
   
-} 
-  }
-step_number++;
+  step_number++;
   if(step_number > 3){
     step_number = 0;
-  }
+    }
+  delay(2);
+}
 }
