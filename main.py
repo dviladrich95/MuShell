810e347f3d0fa93ab0ab8dfedd9648a23b0a3ec8
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-03-30 02:34:44 +0200

    cleanup and file restructuring

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import cv2 as cv
-from matplotlib import pyplot as plt
-from scipy.interpolate import interp1d
-import numpy as np
-from scipy.fft import fft
-import math
-import os
-from midiutil import MIDIFile
-# import pandas as pd
-
-from utils_opencv_midi import setup, count_objects, show_boxes, qshow_boxes, quantize_image, get_boxes, quantize_box, contour2fourier
-from utils_scale import get_scale_cents_and_root, make_exp_scale_list, qbox_list2midi
-
-
-
+from utils_opencv_midi import setup
 
 if __name__ == '__main__':
-    #'conus_textile_lightbox_thresh_strip_dots',
     file_name_list = [
                       'schnecke10_thresh_strip_half_size',]
                       # 'schnecke2_thresh_strip',
@@ -24,18 +9,14 @@ if __name__ == '__main__':
                       # 'schnecke8_thresh_strip',
                       # 'schnecke9_thresh_strip']
 
+    scale_file_name_list = [
+                            'asmaroneng_pelog']
     # 'balafon_1',
     # 'a_major_natural_equal_temperament',
     # 'a_minor_natural_equal_temperament',
-    # 'asmaroneng_pelog',
-    scale_file_name_list = [
-                            'asmaroneng_pelog']
-
-    # setup('schnecke1_thresh_strip', 'balafon_1', note_num=12,
-    #       beat_num=400, root_note=69, forced_duration=1)
 
     for file_name in file_name_list:
         print(file_name)
         for scale_file_name in scale_file_name_list:
             setup(file_name, scale_file_name, note_num=12,
-                  beat_num=800, root_note=69, forced_duration=1)
+                  beat_num=800, root_note=69)
--- utils_opencv_midi.py
+++ utils_opencv_midi.py
@@ -1,18 +1,12 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy.interpolate import interp1d
 from scipy.fft import fft
-import math
 import os
-from midiutil import MIDIFile
 from utils_scale import get_scale_cents_and_root, make_exp_scale_list, qbox_list2midi
 
 def setup(file_name,scale_file_name,note_num = 8,
           beat_num = 400,root_note = 69, forced_duration = 1):
-
-    # img=cv.imread("img.png")
-    # img_thresh=threshold_test(img)
 
     file_name = file_name
     scale_file_name = scale_file_name
@@ -31,34 +25,20 @@ def setup(file_name,scale_file_name,note_num = 8,
     img_thresh = cv.threshold(img_thresh_rgb_flipped, 127, 255, cv.THRESH_BINARY)[1]
     img_shape = img_thresh.shape
 
-    # print("Number of foreground objects", label_count)
-    # cv.imshow("Connected Components", label_image)
-    # cv.waitKey(0)
-
     contours, _ = cv.findContours(img_thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
     box_list = get_boxes(contours)
-    # note_movie(img_thresh, box_list, beat_num, bpm, forced_duration, frame_rate=1)
-
     img_boxes = show_boxes(img_thresh, box_list)
-    #img_output_file = os.path.join(os.getcwd(),'Images','boxes','boxes_'+ midi_str + '.png')
-    #cv.imwrite(img_output_file, np.flipud(img_boxes))
-
     scale = get_scale_cents_and_root(scale_file_name+'.scl')
-
     exp_scale_list = make_exp_scale_list(scale, note_num)
 
-    qbox_list, qbox_list2, qparam_list = quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,forced_duration=forced_duration)
+    qbox_list, qbox_list2, qparam_list = quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num)
 
-    import pandas as pd
     output_file = os.path.join(os.getcwd(), 'midi_files', csv_str)
-    #pd.DataFrame(qbox_list[:,0]).to_csv(output_file+'_0'+'.csv', header=None, index=None)
-    #pd.DataFrame(qbox_list[:,1]).to_csv(output_file+'_1'+'.csv', header=None, index=None)
     np.flip(qbox_list[:, 0]).tofile(output_file+'_0'+'.csv',sep=',')
     np.flip(qbox_list[:, 1]).tofile(output_file+'_1'+'.csv',sep=',')
 
-    qbox_list2midi(qbox_list,root_note, exp_scale_list, midi_str, forced_duration=forced_duration)
-
+    qbox_list2midi(qbox_list,root_note, exp_scale_list, midi_str)
 
     img_qboxes = qshow_boxes(img_thresh, qbox_list2, qparam_list)
 
@@ -69,30 +49,6 @@ def setup(file_name,scale_file_name,note_num = 8,
     cv.waitKey(0)
     qimg_both_output_file = os.path.join(os.getcwd(),'Images','boxes','q_both_boxes_'+ midi_str + '.png')
     cv.imwrite(qimg_both_output_file, np.flipud(img_boxes+img_qboxes))
-
-    # label_count, label_image = count_objects(img_thresh)
-    # label_count, label_image = quantize_image(img_thresh,box_list,qbox_list)
-
-    # box_params=get_boxes(img)
-    # midi_sequence=midi_convert(box_params)
-
-def threshold_test(img):
-    """
-    Test function that converts RGB image to thresholded counterpart, not really used right now
-    :param img: image to be thresholded
-    :return:
-    """
-    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    img_lap = cv.Laplacian(img_gray, cv.CV_64F, ksize=5)
-    abs_sobel64f = np.absolute(img_lap)
-    sobel_8u = np.uint8(abs_sobel64f)
-    blurred = cv.blur(sobel_8u, (3, 3))
-    thresh, output_binthresh = cv.threshold(blurred, 28, 255, cv.THRESH_BINARY)
-
-    output_adapthresh = cv.adaptiveThreshold(img_gray, 255.0, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, -20.0)
-    cv.imshow("Adaptive Thresholding", output_adapthresh)
-    cv.waitKey(0)
-    return output_adapthresh
 
 
 def count_objects(img_thresh):
@@ -154,36 +110,11 @@ def qshow_boxes(img_thresh, qbox_list,qparam_list, color=(0, 255, 0)):
     """
     black_img = np.zeros(img_thresh.shape, np.uint8)
     img_thresh_rgb = cv.cvtColor(black_img, cv.COLOR_GRAY2RGB)
-    qp=qparam_list[0]
-    qt=qparam_list[1]
     for box in qbox_list:
         top_left = (int(box[0]), int(box[1]))
         bottom_right = (int(box[0] + box[2]), int(box[1] + box[3]))
         cv.rectangle(img_thresh_rgb, top_left, bottom_right, color, -1)
     return img_thresh_rgb
-
-def quantize_image(img_thresh, box_list, qbox_list):
-    """
-    Counts the number of objects in the binary image
-    :param box_list:
-    :param qbox_list:
-    :param img_thresh:
-    :return:
-    """
-    box_diff_list = (box_list - qbox_list) // 1
-
-    label_image = img_thresh.copy()
-    label_count = 0
-    print(label_image.shape)
-    rows, cols = label_image.shape
-    for j in range(rows):
-        for i in range(cols):
-            pixel = label_image[j, i]
-            if 255 == pixel:
-                label_count += 1
-                cv.floodFill(label_image, None, (i, j), label_count)
-
-    return label_count, label_image
 
 
 def get_boxes(contours):
@@ -202,7 +133,7 @@ def get_boxes(contours):
     return box_list
 
 
-def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_pixel_units=False,forced_duration=0):
+def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_pixel_units=False):
     """
     Quantizes the time coordinate (y coordinate) of the picture
     :param box_list:
@@ -210,8 +141,8 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
     :param time_divisions:
     :return:
     """
-    qbox_list = np.zeros(box_list.shape)
 
+    qbox_list = np.zeros(box_list.shape)
 
     img_height = img_shape[0]
     img_width = img_shape[1]
@@ -245,7 +176,6 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
     qbox_list[:,2] = np.ceil(qduration_list)
 
     means = []
-    qbox_unique = np.unique(qbox_list[:,0], return_counts=True)
     for i in reversed(np.unique(qbox_list[:,0])):
         tmp = qbox_list[np.where(qbox_list[:,0] == i)]
         tmp[:,2] = np.mean(tmp[:, 2], dtype=int)
@@ -253,10 +183,6 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
 
     duration_mean = np.concatenate(means, axis=0)
     qbox_list[:, 2] = duration_mean[:, 2]
-
-    #if forced_duration:
-        #qbox_list[:, 2] = int(forced_duration)
-
 
     if return_pixel_units:
         qbox_list[:, 0] = note_list
@@ -270,30 +196,3 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
     qbox_list2[:,1] = qbox_list[:,1]*quant_param
 
     return qbox_list, qbox_list2, qparam_list
-
-
-def contour2fourier(contours, n=100000,interpoints=100):
-    """
-    Convert contour pixel list into fourier descriptor
-    """
-    descriptor_list = []
-    for i,contour in enumerate(contours):
-        #f_contour = interp1d(contour[0], contour[1], kind='cubic')
-        #contour_norm = f_contour(100)
-
-        x= np.linspace(0.0, 2* np.pi*n)/n
-        #c1 = np.sin(30.0 * 2.0 * np.pi * x) + 1j * np.sin(30.0 * 2.0 * np.pi * x)
-        #c2 = np.cos(60.0 * 2.0 * np.pi * x) + 1j * np.sin(60.0 * 2.0 * np.pi * x)
-        #complex_ctr = c1 + c2
-        complex_ctr = contour[:, 0, 0] + 1j * contour[:, 0, 1]
-        #descriptor_complex = np.fft.fft(complex_ctr, axis=0, n=n)
-        descriptor_complex = fft(complex_ctr, axis=0, n=n)
-        descriptor_abs = np.abs(descriptor_complex)
-        descriptor = descriptor_abs[1:] / descriptor_abs[1]
-        descriptor_list.append(descriptor_abs)
-
-        plt.title("Descriptor {} with {} pixels ".format(i,len(contour)))
-        plt.plot(range(1, n), descriptor[:n])
-        plt.show()
-
-    return descriptor_list
