9267aaa636a529c0518505d65be8a68121cf9012
dviladrich95 <daniel.viladrich.h@gmail.com>
2022-03-30 03:07:11 +0200

    cleanup, removed unnecessary functions

--- utils_opencv_midi.py
+++ utils_opencv_midi.py
@@ -1,26 +1,21 @@
 import cv2 as cv
 import numpy as np
-from matplotlib import pyplot as plt
-from scipy.fft import fft
 import os
 from utils_scale import get_scale_cents_and_root, make_exp_scale_list, qbox_list2midi
 
-def setup(file_name,scale_file_name,note_num = 8,
-          beat_num = 400,root_note = 69, forced_duration = 1):
 
+def setup(file_name, scale_file_name, note_num=8, beat_num=400, root_note=69):
     file_name = file_name
     scale_file_name = scale_file_name
 
-    note_num = note_num # number of pitch subdivisions
-    beat_num = beat_num # number of time subdivisions in beats
-    root_note = root_note #midi number corresponding to where to start. 74 starts at c5, A4 at 69
-    forced_duration = forced_duration
-    bpm = 120
+    note_num = note_num  # number of pitch subdivisions
+    beat_num = beat_num  # number of time subdivisions in beats
+    root_note = root_note  # midi number corresponding to where to start. 74 starts at c5, A4 at 69
+    # bpm = 120
     midi_str = file_name + '_' + scale_file_name + '_' + str(note_num) + '_' + str(beat_num) + '.mid'
     csv_str = file_name + '_' + scale_file_name + '_' + str(note_num) + '_' + str(beat_num)
 
-
-    img_thresh_rgb = cv.imread(os.path.join('Images',file_name+'.png'), 0)
+    img_thresh_rgb = cv.imread(os.path.join('Images', file_name + '.png'), 0)
     img_thresh_rgb_flipped = np.flipud(img_thresh_rgb)
     img_thresh = cv.threshold(img_thresh_rgb_flipped, 127, 255, cv.THRESH_BINARY)[1]
     img_shape = img_thresh.shape
@@ -29,26 +24,26 @@ def setup(file_name,scale_file_name,note_num = 8,
 
     box_list = get_boxes(contours)
     img_boxes = show_boxes(img_thresh, box_list)
-    scale = get_scale_cents_and_root(scale_file_name+'.scl')
+    scale = get_scale_cents_and_root(scale_file_name + '.scl')
     exp_scale_list = make_exp_scale_list(scale, note_num)
 
     qbox_list, qbox_list2, qparam_list = quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num)
 
     output_file = os.path.join(os.getcwd(), 'midi_files', csv_str)
-    np.flip(qbox_list[:, 0]).tofile(output_file+'_0'+'.csv',sep=',')
-    np.flip(qbox_list[:, 1]).tofile(output_file+'_1'+'.csv',sep=',')
+    np.flip(qbox_list[:, 0]).tofile(output_file + '_0' + '.csv', sep=',')
+    np.flip(qbox_list[:, 1]).tofile(output_file + '_1' + '.csv', sep=',')
 
-    qbox_list2midi(qbox_list,root_note, exp_scale_list, midi_str)
+    qbox_list2midi(qbox_list, root_note, exp_scale_list, midi_str)
 
     img_qboxes = qshow_boxes(img_thresh, qbox_list2, qparam_list)
 
-    qimg_output_file = os.path.join(os.getcwd(),'Images','boxes','q_boxes_'+ midi_str + '.png')
-    cv.imwrite(qimg_output_file,np.flipud(img_qboxes))
+    qimg_output_file = os.path.join(os.getcwd(), 'Images', 'boxes', 'q_boxes_' + midi_str + '.png')
+    cv.imwrite(qimg_output_file, np.flipud(img_qboxes))
 
-    cv.imshow("Quantized box differences", img_boxes+img_qboxes)
+    cv.imshow("Quantized box differences", img_boxes + img_qboxes)
     cv.waitKey(0)
-    qimg_both_output_file = os.path.join(os.getcwd(),'Images','boxes','q_both_boxes_'+ midi_str + '.png')
-    cv.imwrite(qimg_both_output_file, np.flipud(img_boxes+img_qboxes))
+    qimg_both_output_file = os.path.join(os.getcwd(), 'Images', 'boxes', 'q_both_boxes_' + midi_str + '.png')
+    cv.imwrite(qimg_both_output_file, np.flipud(img_boxes + img_qboxes))
 
 
 def count_objects(img_thresh):
@@ -70,28 +65,13 @@ def count_objects(img_thresh):
 
     return label_count, label_image
 
-def note_movie(img_thresh,box_list,beat_num,bpm,duration,frame_rate=1):
-    """
-    writes frames to disk highlighting played notes at each moment
-    :param img_thresh: thresholded image
-    :return:
-    """
-    fpbeat = 60.0 / bpm * frame_rate
-    frame_num = int(beat_num*fpbeat)
-    for frame_i in range(frame_num):
-        frame = np.zeros(img_thresh.shape)
-        for box in box_list:
-            frame_box_diff = abs(frame_i/(60.0/bpm*frame_rate)-box[1])
-            if abs(frame_i/fpbeat-box[1]) < duration:
-                frame[int(box[1]):int(box[1] + box[3]),int(box[0]):int(box[0] + box[2])] = img_thresh[ int(box[1]):int(box[1] + box[3]),int(box[0]):int(box[0] + box[2])]
-        cv.imwrite("frame{}.png".format(frame_i),frame)
-    return
 
 def show_boxes(img_thresh, box_list, color=(255, 0, 0)):
     """
     Show a plot with boxes around each dot
     :param img_thresh:
-    :param boxes:
+    :param box_list:
+    :param color:
     :return:
     """
     img_thresh_rgb = cv.cvtColor(img_thresh.copy(), cv.COLOR_GRAY2RGB)
@@ -101,11 +81,14 @@ def show_boxes(img_thresh, box_list, color=(255, 0, 0)):
         cv.rectangle(img_thresh_rgb, top_left, bottom_right, color, -1)
     return img_thresh_rgb
 
-def qshow_boxes(img_thresh, qbox_list,qparam_list, color=(0, 255, 0)):
+
+def qshow_boxes(img_thresh, qbox_list, qparam_list, color=(0, 255, 0)):
     """
     Show a plot with boxes around each dot
     :param img_thresh:
-    :param boxes:
+    :param qbox_list:
+    :param qparam_list:
+    :param color:
     :return:
     """
     black_img = np.zeros(img_thresh.shape, np.uint8)
@@ -133,12 +116,15 @@ def get_boxes(contours):
     return box_list
 
 
-def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_pixel_units=False):
+def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num, return_pixel_units=False):
     """
     Quantizes the time coordinate (y coordinate) of the picture
     :param box_list:
-    :param img_height:
-    :param time_divisions:
+    :param img_shape:
+    :param exp_scale_list:
+    :param note_num:
+    :param beat_num:
+    :param return_pixel_units:
     :return:
     """
 
@@ -147,10 +133,10 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
     img_height = img_shape[0]
     img_width = img_shape[1]
 
-    qexp_scale_list=[round(note/100) for note in exp_scale_list]
+    qexp_scale_list = [round(note / 100) for note in exp_scale_list]
 
-    #quantize the x (pitch) coordinate by converting it to the note index of the nearest scale note
-    pitch_norm_param = img_width / qexp_scale_list[-1] # quantization parameter
+    # quantize the x (pitch) coordinate by converting it to the note index of the nearest scale note
+    pitch_norm_param = img_width / qexp_scale_list[-1]  # quantization parameter
     pitch_list = box_list[:, 0]
     pitch_list_normed = np.asarray(pitch_list) / pitch_norm_param
     pitch_list_normed_flat = np.repeat(pitch_list_normed, note_num)
@@ -159,7 +145,7 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
     exp_scale_list_tile = np.reshape(exp_scale_list_flat, (len(pitch_list), note_num))
     note_ind_list = np.argmin(np.abs(pitch_list_normed_tile - exp_scale_list_tile), axis=1)
 
-    #qbox_list[:,0] = note_ind_list
+    # qbox_list[:,0] = note_ind_list
     note_list = [int(qexp_scale_list[i]) for i in note_ind_list]
     qbox_list[:, 0] = note_list
     qbox_list[:, 3] = np.asarray(box_list[:, 3]) / pitch_norm_param
@@ -167,18 +153,16 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
     # quantize the y (time) axis
     quant_param = img_height / beat_num
     qtime_list = (box_list[:, 1] / quant_param).astype(int)
-    #qtime_list = qtime_list_boxnum * int(quant_param)
 
     qduration_list = box_list[:, 3] / quant_param
-    #qtime_list = qtime_list_boxnum * int(quant_param)
 
-    qbox_list[:,1] = qtime_list
-    qbox_list[:,2] = np.ceil(qduration_list)
+    qbox_list[:, 1] = qtime_list
+    qbox_list[:, 2] = np.ceil(qduration_list)
 
     means = []
-    for i in reversed(np.unique(qbox_list[:,0])):
-        tmp = qbox_list[np.where(qbox_list[:,0] == i)]
-        tmp[:,2] = np.mean(tmp[:, 2], dtype=int)
+    for i in reversed(np.unique(qbox_list[:, 0])):
+        tmp = qbox_list[np.where(qbox_list[:, 0] == i)]
+        tmp[:, 2] = np.mean(tmp[:, 2], dtype=int)
         means.append(tmp)
 
     duration_mean = np.concatenate(means, axis=0)
@@ -190,9 +174,9 @@ def quantize_box(box_list, img_shape, exp_scale_list, note_num, beat_num,return_
 
     qbox_list = qbox_list.astype(int)
 
-    qparam_list = [pitch_norm_param,quant_param]
+    qparam_list = [pitch_norm_param, quant_param]
     qbox_list2 = box_list
-    qbox_list2[:,0] = qbox_list[:,0]*pitch_norm_param
-    qbox_list2[:,1] = qbox_list[:,1]*quant_param
+    qbox_list2[:, 0] = qbox_list[:, 0] * pitch_norm_param
+    qbox_list2[:, 1] = qbox_list[:, 1] * quant_param
 
     return qbox_list, qbox_list2, qparam_list
--- utils_scale.py
+++ utils_scale.py
@@ -1,13 +1,7 @@
-import cv2 as cv
-from matplotlib import pyplot as plt
-from scipy.interpolate import interp1d
-import numpy as np
-from scipy.fft import fft
 import math
 import os
 from midiutil import MIDIFile
 from fractions import Fraction
-# import pandas as pd
 
 
 def get_scale_paths():
@@ -26,21 +20,23 @@ def get_scale_cents_and_root(scale_name):
     """
     cwd = os.getcwd()
     scale_directory = os.path.join(cwd, "scales")
-    with open(os.path.join(scale_directory,scale_name), "r") as scale_file:
+    with open(os.path.join(scale_directory, scale_name), "r") as scale_file:
         scale_str = scale_file.read().replace('\n', ' ')
 
         scale_str_list = scale_str.split('!')
 
         scale_list_dirty = scale_str_list[-1].split(' ')
-        scale_list_noconv=list(filter(None, scale_list_dirty))
-        scale_list=[]
+        scale_list_noconv = list(filter(None, scale_list_dirty))
+        scale_list = []
         for note in scale_list_noconv:
             if '/' in note:
-                scale_list.append(math.log2(float(Fraction(note)))*1200) # numbers expressed as ratios need to be converted into cents first
+                scale_list.append(math.log2(
+                    float(Fraction(note))) * 1200)  # numbers expressed as ratios need to be converted into cents first
             else:
                 scale_list.append(float(note))
 
     return scale_list
+
 
 def make_exp_scale_list(scale, note_num):
     """
@@ -49,14 +45,14 @@ def make_exp_scale_list(scale, note_num):
     :param note_num: number of notes to use, the scale repeates until all notes in the range are used
     :return: list of notes using the given scale
     """
-    scale_range = scale[-1] # range of the scale, will always be 1200 for scales that span a whole octave
-    exp_scale_list=[0.0] # initial note was not part of the scale, is added now
-    for i in range(note_num-1):
-        exp_scale_list.append(scale[i%len(scale)]+i//(len(scale))*scale_range)
+    scale_range = scale[-1]  # range of the scale, will always be 1200 for scales that span a whole octave
+    exp_scale_list = [0.0]  # initial note was not part of the scale, is added now
+    for i in range(note_num - 1):
+        exp_scale_list.append(scale[i % len(scale)] + i // (len(scale)) * scale_range)
     return exp_scale_list
 
 
-def qbox_list2midi(qbox_list,root_note,exp_scale_list,midi_str,forced_duration=0):
+def qbox_list2midi(qbox_list, root_note, exp_scale_list, midi_str, forced_duration=0):
     """
     Converts each list of box parameters into MIDI format (note, duration, loudness) using the MIDIutil module
     """
@@ -77,7 +73,8 @@ def qbox_list2midi(qbox_list,root_note,exp_scale_list,midi_str,forced_duration=0
     midi_file.changeTuningProgram(0, 0, 0, 0)
     # Add some notes
     for note, time, duration, _ in qbox_list:
-        midi_file.addNote(track, channel, note+root_note, time, duration, volume) # time and duration measured in beats
+        midi_file.addNote(track, channel, note + root_note, time, duration,
+                          volume)  # time and duration measured in beats
 
     # Write to disk
     with open(output_file, "wb") as out_file:
@@ -92,5 +89,3 @@ def qbox_list2midi(qbox_list,root_note,exp_scale_list,midi_str,forced_duration=0
 
             with open(output_file, "wb") as out_file:
                 midi_file.writeFile(out_file)
-
-
